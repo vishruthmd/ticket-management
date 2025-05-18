b70f592fb8b56beb70479010318d8514db0d6345
vishruthmd <vishruthmd18@gmail.com>
2025-05-18 17:50:19 +0530

    cors policy added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // router imports
 import authRoutes from "./routes/auth.routes.js";
@@ -11,9 +12,11 @@ dotenv.config();
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-// app.use(cors({
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174"], // Allow frontend origin
+  credentials: true,               // If you’re sending cookies or headers
+}));
 
-// }));
 app.use(express.json());
 app.use(cookieParser());
 
